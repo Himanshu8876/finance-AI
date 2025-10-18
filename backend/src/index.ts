13532b67464c9dc612226c83eb1e0a56705276a7
Himanshu8876 <garghimanshu778@gmail.com>
2025-10-18 18:45:26 +0530

    origin changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://curious-sunflower-e63115.netlify.app/", 
     credentials: true,              
   })
 );
